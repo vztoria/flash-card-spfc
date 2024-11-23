79af11b4558e4643dcf2259af3b8963fc1ade333
vztoria <00001138291754sp@al.educacao.sp.gov.br>
2024-11-23 15:32:41 -0300

    Add files via upload

---
+++ flash card v/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'SPFC',
+    'Em que ano o São Paulo Futebol Clube foi fundado?',
+    'Em 25 de janeiro de 1930.'
+)
+
+criaCartao(
+    'SPFC',
+    'Quantos títulos da Libertadores o São Paulo possui?',
+    'Três títulos (1992, 1993 e 2005).'
+)
+
+criaCartao(
+    'SPFC',
+    'Qual é o nome do estádio do São Paulo?',
+    'Estádio do Morumbi, também chamado de Estádio Cícero Pompeu de Toledo.'
+)
+
+criaCartao(
+    'SPFC',
+    'Qual é o apelido do São Paulo que faz referência a um tricolor?',
+    ' O apelido é "Tricolor Paulista".'
+)
